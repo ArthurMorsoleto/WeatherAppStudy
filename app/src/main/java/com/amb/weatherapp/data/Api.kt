0a59c2b46b4efdcb1d8bc48c983944f68629ca3c
Arthur M Batista <arthur.morsoleto@gmail.com>
2024-07-22 22:52:35 -0300

    add endpoint and query params to get weather data method

--- app/src/main/java/com/amb/weatherapp/data/Api.kt
+++ app/src/main/java/com/amb/weatherapp/data/Api.kt
@@ -1,12 +1,24 @@
 package com.amb.weatherapp.data
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 /**
- *
+ *  Base api interface for fetching data from https://open-meteo.com/
  */
 interface Api {
 
     /**
-     *
+     *  Gets Weather Data Information based on user location
      */
-    suspend fun getWeatherData()
+    @GET(WEATHER_ENDPOINT)
+    suspend fun getWeatherData(
+        @Query("latitude") lat: Double,
+        @Query("longitude") long: Double
+    )
+
+    companion object {
+        private const val WEATHER_ENDPOINT =
+            "v1/forecast?hourly=temperature_2m,weathercode,relativehumidity_2m,windspeed_10m,pressure_msl"
+    }
 }
